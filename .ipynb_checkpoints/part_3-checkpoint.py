43552be13a88ff019631721d94c5683abd9c0ed8
Carlos Cano <carloscano@pop-os.localdomain>
2023-02-05 22:21:14 +0000

    Code review v1

--- .ipynb_checkpoints/part_3-checkpoint.py
+++ .ipynb_checkpoints/part_3-checkpoint.py
@@ -1,67 +1,66 @@
 def crop_simulation(crop_name, PAR_results, dict_coor):
-    
 
-    #Define Location
-    
-    coor_key = ['Latitude', 'Longitude', 'Name', 'Altitude']
+    # Define Location
+
+    coor_key = ["Latitude", "Longitude", "Name", "Altitude"]
     for key in coor_key:
         if (key in dict_coor) == False:
             print("No esta la clave " + key)
-    
-    latitude = dict_coor['Latitude']
-    longitude = dict_coor['Longitude']
-    
-    #Define Crops Calendar
-    
+
+    latitude = dict_coor["Latitude"]
+    longitude = dict_coor["Longitude"]
+
+    # Define Crops Calendar
+
     crop_year_int = PAR_results.index[1].year
     crop_year_str = str(crop_year_int)
-    
-    if crop_name == 'potato':
-        crop_name = 'potato'
-        variety_name = 'Potato_701'
-        campaign_start_date = 'xxxx-04-01'
-        campaign_start_date = campaign_start_date.replace('xxxx', crop_year_str)
-        emergence_date = 'xxxx-04-30'
-        emergence_date = emergence_date.replace('xxxx', crop_year_str)
-        harvest_date = 'xxxx-09-10'
+
+    if crop_name == "potato":
+        crop_name = "potato"
+        variety_name = "Potato_701"
+        campaign_start_date = "xxxx-04-01"
+        campaign_start_date = campaign_start_date.replace("xxxx", crop_year_str)
+        emergence_date = "xxxx-04-30"
+        emergence_date = emergence_date.replace("xxxx", crop_year_str)
+        harvest_date = "xxxx-09-10"
         harvest_date = harvest_date.replace("xxxx", crop_year_str)
         max_duration = 160
-        
-    elif crop_name == 'maize':
-        crop_name = 'maize'
-        variety_name = 'Grain_maize_201'
-        campaign_start_date = 'xxxx-04-01'
-        campaign_start_date = campaign_start_date.replace('xxxx', crop_year_str)
-        emergence_date = 'xxxx-04-30'
-        emergence_date = emergence_date.replace('xxxx', crop_year_str)
-        harvest_date = 'xxxx-11-20'
+
+    elif crop_name == "maize":
+        crop_name = "maize"
+        variety_name = "Grain_maize_201"
+        campaign_start_date = "xxxx-04-01"
+        campaign_start_date = campaign_start_date.replace("xxxx", crop_year_str)
+        emergence_date = "xxxx-04-30"
+        emergence_date = emergence_date.replace("xxxx", crop_year_str)
+        harvest_date = "xxxx-11-20"
         harvest_date = harvest_date.replace("xxxx", crop_year_str)
         max_duration = 250
-        
-    elif crop_name == 'sunflower':
-        crop_name = 'sunflower'
-        variety_name = 'Sunflower_1101'
-        campaign_start_date = 'xxxx-04-01'
-        campaign_start_date = campaign_start_date.replace('xxxx', crop_year_str)
-        emergence_date = 'xxxx-04-20'
-        emergence_date = emergence_date.replace('xxxx', crop_year_str)
-        harvest_date = 'xxxx-10-30'
+
+    elif crop_name == "sunflower":
+        crop_name = "sunflower"
+        variety_name = "Sunflower_1101"
+        campaign_start_date = "xxxx-04-01"
+        campaign_start_date = campaign_start_date.replace("xxxx", crop_year_str)
+        emergence_date = "xxxx-04-20"
+        emergence_date = emergence_date.replace("xxxx", crop_year_str)
+        harvest_date = "xxxx-10-30"
         harvest_date = harvest_date.replace("xxxx", crop_year_str)
         max_duration = 180
-    elif crop_name == 'barley':
-        crop_name = 'barley'
-        variety_name = 'Spring_barley_301'
-        campaign_start_date = 'xxxx-03-01'
-        campaign_start_date = campaign_start_date.replace('xxxx', crop_year_str)
-        emergence_date = 'xxxx-03-31'
-        emergence_date = emergence_date.replace('xxxx', crop_year_str)
-        harvest_date = 'xxxx-09-15'
+    elif crop_name == "barley":
+        crop_name = "barley"
+        variety_name = "Spring_barley_301"
+        campaign_start_date = "xxxx-03-01"
+        campaign_start_date = campaign_start_date.replace("xxxx", crop_year_str)
+        emergence_date = "xxxx-03-31"
+        emergence_date = emergence_date.replace("xxxx", crop_year_str)
+        harvest_date = "xxxx-09-15"
         harvest_date = harvest_date.replace("xxxx", crop_year_str)
         max_duration = 180
 
-        
     # Here we define the agromanagement for the selected crop
-    import yaml 
+    import yaml
+
     agro_yaml = """
     - {start}:
         CropCalendar:
@@ -74,85 +73,96 @@ def crop_simulation(crop_name, PAR_results, dict_coor):
             max_duration: {maxdur}
         TimedEvents: null
         StateEvents: null
-    """.format(cname=crop_name, vname=variety_name, 
-               start=campaign_start_date, startdate=emergence_date, 
-               enddate=harvest_date, maxdur=max_duration)
+    """.format(
+        cname=crop_name,
+        vname=variety_name,
+        start=campaign_start_date,
+        startdate=emergence_date,
+        enddate=harvest_date,
+        maxdur=max_duration,
+    )
     agro = yaml.safe_load(agro_yaml)
     # print(agro_yaml)
-    
-    #Weather
-    wdp = NASAPowerWeatherDataProvider(latitude=latitude, longitude=longitude, force_update=False, ETmodel='PM')
-    
+
+    # Weather
+    wdp = NASAPowerWeatherDataProvider(
+        latitude=latitude, longitude=longitude, force_update=False, ETmodel="PM"
+    )
+
     # Parameter sets for crop, soil and site
     # Standard crop parameter library
     cropd = YAMLCropDataProvider()
     # We don't need soil for potential production, so we use dummy values
     soild = DummySoilDataProvider()
     # Some site parameters
-    sited = WOFOST72SiteDataProvider(WAV=50, CO2=360.)
+    sited = WOFOST72SiteDataProvider(WAV=50, CO2=360.0)
 
-    # Retrieve all parameters in the form of a single object. 
+    # Retrieve all parameters in the form of a single object.
     # In order to see all parameters for the selected crop already, we
-    # synchronise data provider cropd with the crop/variety: 
+    # synchronise data provider cropd with the crop/variety:
     firstkey = list(agro[0])[0]
-    cropcalendar = agro[0][firstkey]['CropCalendar'] 
-    cropd.set_active_crop(cropcalendar['crop_name'], cropcalendar['variety_name'])
+    cropcalendar = agro[0][firstkey]["CropCalendar"]
+    cropd.set_active_crop(cropcalendar["crop_name"], cropcalendar["variety_name"])
     params = ParameterProvider(cropdata=cropd, sitedata=sited, soildata=soild)
-    
-    #Open-field calculation. This part is necessary to be able to compare the crop production in open field versus other shaded conditions.
-    
+
+    # Open-field calculation. This part is necessary to be able to compare the crop production in open field versus other shaded conditions.
+
     wdp_of = wdp
     for counter_of in range(len(PAR_results.index)):
-        wdp_of(PAR_results.index[counter_of]).IRRAD = PAR_results['PAR_absorption_tot_of'][counter_of]
-    
+        wdp_of(PAR_results.index[counter_of]).IRRAD = PAR_results[
+            "PAR_absorption_tot_of"
+        ][counter_of]
+
     wfpp = Wofost71_PP(params, wdp_of, agro)
     wfpp.run_till_terminate()
     summary_output = wfpp.get_summary_output()
-    
-    total_biomass_of = summary_output[0]['TAGP']
-    harvestable_product_of = summary_output[0]['TWSO']
-    
-    #Under PV calculation
-    
-#     wdp_pv = wdp
-#     for counter_pv in range(len(PAR_results.index)):
-#         wdp_pv(PAR_results.index[counter_pv]).IRRAD = PAR_results['PAR_absorption_tot'][counter_pv]
-    
-#     wfpp = Wofost71_PP(params, wdp_pv, agro)
-#     wfpp.run_till_terminate()
-#     summary_output = wfpp.get_summary_output()
-    
-#     total_biomass_pv = summary_output[0]['TAGP']
-#     harvestable_product_pv = summary_output[0]['TWSO']
-    
-    
-       
-#     ---------------------------------------CAMBIOS-----------------------------------------
+
+    total_biomass_of = summary_output[0]["TAGP"]
+    harvestable_product_of = summary_output[0]["TWSO"]
+
+    # Under PV calculation
+
+    #     wdp_pv = wdp
+    #     for counter_pv in range(len(PAR_results.index)):
+    #         wdp_pv(PAR_results.index[counter_pv]).IRRAD = PAR_results['PAR_absorption_tot'][counter_pv]
+
+    #     wfpp = Wofost71_PP(params, wdp_pv, agro)
+    #     wfpp.run_till_terminate()
+    #     summary_output = wfpp.get_summary_output()
+
+    #     total_biomass_pv = summary_output[0]['TAGP']
+    #     harvestable_product_pv = summary_output[0]['TWSO']
+
+    #     ---------------------------------------CAMBIOS-----------------------------------------
     # We input the calculated absorbed irradiance data from part 2 for both conventional cells and DSSCs.
     wdp_conv = wdp
     for counter_conv in range(len(PAR_results.index)):
-        wdp_conv(PAR_results.index[counter_conv]).IRRAD = PAR_results['PAR_absorption_tot_conv'][counter_conv]
-    
-    wfpp = Wofost72_PP(params, wdp_conv, agro)    #Wofost72_PP (potencial production). It only takes into account the effect of irradiance, assuming that the other parameters that influence growth are always optimal. This way, we can compare the different results while only considering the irradiance, which is what is being investigated in this case.
+        wdp_conv(PAR_results.index[counter_conv]).IRRAD = PAR_results[
+            "PAR_absorption_tot_conv"
+        ][counter_conv]
+
+    wfpp = Wofost72_PP(
+        params, wdp_conv, agro
+    )  # Wofost72_PP (potencial production). It only takes into account the effect of irradiance, assuming that the other parameters that influence growth are always optimal. This way, we can compare the different results while only considering the irradiance, which is what is being investigated in this case.
     wfpp.run_till_terminate()
     summary_output = wfpp.get_summary_output()
-    
-    total_biomass_conv = summary_output[0]['TAGP']
-    harvestable_product_conv = summary_output[0]['TWSO']
-    
+
+    total_biomass_conv = summary_output[0]["TAGP"]
+    harvestable_product_conv = summary_output[0]["TWSO"]
+
     wdp_dssc = wdp
     for counter_dssc in range(len(PAR_results.index)):
-        wdp_dssc(PAR_results.index[counter_dssc]).IRRAD = PAR_results['PAR_absorption_tot_dssc'][counter_dssc]
-    
+        wdp_dssc(PAR_results.index[counter_dssc]).IRRAD = PAR_results[
+            "PAR_absorption_tot_dssc"
+        ][counter_dssc]
+
     wfpp = Wofost72_PP(params, wdp_dssc, agro)
     wfpp.run_till_terminate()
     summary_output = wfpp.get_summary_output()
-    
-    total_biomass_dssc = summary_output[0]['TAGP']
-    harvestable_product_dssc = summary_output[0]['TWSO']
 
+    total_biomass_dssc = summary_output[0]["TAGP"]
+    harvestable_product_dssc = summary_output[0]["TWSO"]
 
+    # ------------------------------------------------------------------------------------------
 
-# ------------------------------------------------------------------------------------------
-    
     return harvestable_product_of, harvestable_product_conv, harvestable_product_dssc
--- part_2.py
+++ part_2.py
@@ -1,82 +1,68 @@
-def PAR_plants(spectrum_df, weather_index, tot_coeff):
-    
-    # We store the spectrogram and decompose it by wavelength.
-   
-    # Rename the input of spectrums
-    
-    hourly_spectral_conv = spectrum_df['ghi_spectrum_col_conv']
-    hourly_spectral_dssc = spectrum_df['ghi_spectrum_col_dssc']
-    hourly_spectral_of = spectrum_df['open_field_spectrum']
-    
-    
+import pandas as pd
 
-   
-    #We create a dataframe to store the absorbed PAR taking into account the ABSORBED PAR AND ACTION PAR (TOTAL QUANTUM YIELD).
-    
+
+def PAR_plants(spectrum_df, weather_index, tot_coeff):
+    # We store the spectrogram and decompose it by wavelength.
+
+    # Rename the input of spectrums
+
+    hourly_spectral_conv = spectrum_df["ghi_spectrum_col_conv"]
+    hourly_spectral_dssc = spectrum_df["ghi_spectrum_col_dssc"]
+    hourly_spectral_of = spectrum_df["open_field_spectrum"]
+
+    # We create a dataframe to store the absorbed PAR taking into account the ABSORBED PAR AND ACTION PAR (TOTAL QUANTUM YIELD).
+
     PAR_hourly = pd.DataFrame()
     PAR_hourly.index = weather_index
-    
+
     # Here we will take the hourly spectra that reach the crop in 3 cases: conventional cells, DSSCs, and open field. Finally, the crop absorption coefficients at each wavelength will be applied to determine how much of that spectrum the plant actually utilizes.
     PAR_absorption_conv = []
     PAR_absorption_dssc = []
     PAR_absorption_of = []
     for date_index_ab in range(len(hourly_spectral_conv)):
-        PAR_absorption_conv.append(hourly_spectral_conv[date_index_ab]*tot_coeff.reshape(-1))
-        PAR_absorption_dssc.append(hourly_spectral_dssc[date_index_ab]*tot_coeff.reshape(-1))
-        PAR_absorption_of.append(hourly_spectral_of[date_index_ab]*tot_coeff.reshape(-1))
-        
-    PAR_hourly['PAR_absorption_conv'] = PAR_absorption_conv
-    PAR_hourly['PAR_absorption_dssc'] = PAR_absorption_dssc
-    PAR_hourly['PAR_absorption_of'] = PAR_absorption_of
+        PAR_absorption_conv.append(
+            hourly_spectral_conv[date_index_ab] * tot_coeff.reshape(-1)
+        )
+        PAR_absorption_dssc.append(
+            hourly_spectral_dssc[date_index_ab] * tot_coeff.reshape(-1)
+        )
+        PAR_absorption_of.append(
+            hourly_spectral_of[date_index_ab] * tot_coeff.reshape(-1)
+        )
 
+    PAR_hourly["PAR_absorption_conv"] = PAR_absorption_conv
+    PAR_hourly["PAR_absorption_dssc"] = PAR_absorption_dssc
+    PAR_hourly["PAR_absorption_of"] = PAR_absorption_of
 
-# ----------------------------------------------------------------------------------------------
     
-    #Ahora vamos a pasar los resultados a J/m2·day que es como funciona el simulador de cultivos.
-    
-#     #creamos el dataframe donde guardaremos los resultados
-#     PAR_for_daily = pd.DataFrame()
-#     PAR_for_daily.index = weather_index
-    
-#     PAR_absorption_tot = []
-#     PAR_absorption_tot_of = []
-    
-# #     #Se va a calcular la irradiancia total, sumando lo que aportan todas las longitudes de onda y posteriormente se pasa al total diario.
-    
-#     for date_index in range(len(hourly_spectral)):
-#         PAR_absorption_tot.append((PAR_hourly['PAR_absorption'][date_index].sum()) * 3600)
-#         PAR_absorption_tot_of.append((PAR_hourly['PAR_absorption_of'][date_index].sum()) * 3600)
-
-#     PAR_for_daily['PAR_absorption_tot'] = PAR_absorption_tot
-#     PAR_for_daily['PAR_absorption_tot_of'] = PAR_absorption_tot_of
-#     PAR_daily = PAR_for_daily.resample('D').sum()
-#     PAR_equivalent = PAR_daily * 2 #Para contrarestar el 0.5 del simulador de cultivos
-    
-#     -----------------------------------------CAMBIOS ----------------------------------------------
-   
-    #Now we will convert the results to J/m2·day which is how the crop simulator operates.
+    # Now we will convert the results to J/m2·day which is how the crop simulator operates.
     PAR_for_daily = pd.DataFrame()
     PAR_for_daily.index = weather_index
-    
+
     PAR_absorption_tot_conv = []
     PAR_absorption_tot_dssc = []
     PAR_absorption_tot_of = []
-    
-    #The total irradiance will be calculated by summing up the contribution of all wavelengths and then converting it to the daily total.
-    
+
+    # The total irradiance will be calculated by summing up the contribution of all wavelengths and then converting it to the daily total.
+
     for date_index in range(len(hourly_spectral_conv)):
-        PAR_absorption_tot_conv.append((PAR_hourly['PAR_absorption_conv'][date_index].sum()) * 3600)
-        PAR_absorption_tot_dssc.append((PAR_hourly['PAR_absorption_dssc'][date_index].sum()) * 3600)
-        PAR_absorption_tot_of.append((PAR_hourly['PAR_absorption_of'][date_index].sum()) * 3600)
+        PAR_absorption_tot_conv.append(
+            (PAR_hourly["PAR_absorption_conv"][date_index].sum()) * 3600
+        )
+        PAR_absorption_tot_dssc.append(
+            (PAR_hourly["PAR_absorption_dssc"][date_index].sum()) * 3600
+        )
+        PAR_absorption_tot_of.append(
+            (PAR_hourly["PAR_absorption_of"][date_index].sum()) * 3600
+        )
 
-    PAR_for_daily['PAR_absorption_tot_conv'] = PAR_absorption_tot_conv
-    PAR_for_daily['PAR_absorption_tot_dssc'] = PAR_absorption_tot_dssc
-    PAR_for_daily['PAR_absorption_tot_of'] = PAR_absorption_tot_of
-    PAR_daily = PAR_for_daily.resample('D').sum()
-    PAR_equivalent = PAR_daily * 2 #The crop simulator directly estimates a PAR of 1/2 with respect to the total irradiance, without taking into account the shape of the spectrum (since it assumes it always follows the normal distribution). Since in this case we have taken into account the plant's absorption for each wavelength, we are going to nullify the 1/2 factor that the built-in crop simulator has by multiplying our results by 2.
-
+    PAR_for_daily["PAR_absorption_tot_conv"] = PAR_absorption_tot_conv
+    PAR_for_daily["PAR_absorption_tot_dssc"] = PAR_absorption_tot_dssc
+    PAR_for_daily["PAR_absorption_tot_of"] = PAR_absorption_tot_of
+    PAR_daily = PAR_for_daily.resample("D").sum()
+    PAR_equivalent = (
+        PAR_daily * 2
+    )  # The crop simulator directly estimates a PAR of 1/2 with respect to the total irradiance, without taking into account the shape of the spectrum (since it assumes it always follows the normal distribution). Since in this case we have taken into account the plant's absorption for each wavelength, we are going to nullify the 1/2 factor that the built-in crop simulator has by multiplying our results by 2.
 
 
 # -----------------------------------------------------------------------------------------------------
-    
-    
